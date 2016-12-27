5c8313541d528b51b0a335849a60eab7a267ae92
Diego Fincatto <diego@fincatto.com>
2016-12-27 11:21:48 -0200

    Utilizando datas do java8 ao inves de Calendar.

--- src/main/java/com/github/wmixvideo/correios/WSCorreiosCalculador.java
+++ src/main/java/com/github/wmixvideo/correios/WSCorreiosCalculador.java
@@ -5,67 +5,60 @@ import br.com.correios.webservice.calculador.CServico;
 import br.com.correios.webservice.calculador.CalcPrecoPrazoWS;
 import br.com.correios.webservice.calculador.CalcPrecoPrazoWSSoap;
 
-import javax.swing.text.html.Option;
 import javax.xml.ws.BindingProvider;
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public class WSCorreiosCalculador {
 
-    private SimpleDateFormat sd = new SimpleDateFormat("dd/MM/yyyy");
-
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
     private final CalcPrecoPrazoWSSoap calculadora;
-    private final String usuario;
-    private final String senha;
+    private String usuario, senha;
 
     public WSCorreiosCalculador() {
         this.calculadora = new CalcPrecoPrazoWS().getCalcPrecoPrazoWSSoap();
         this.usuario = null;
         this.senha = null;
-        String CONNECT_TIMEOUT = "com.sun.xml.internal.ws.connect.timeout";  //JAXWSProperties.CONNECT_TIMEOUT
-        String REQUEST_TIMEOUT = "com.sun.xml.internal.ws.request.timeout"; //JAXWSProperties.REQUEST_TIMEOUT
-        ((BindingProvider)this.calculadora).getRequestContext().put(CONNECT_TIMEOUT, 3000);
-        ((BindingProvider)this.calculadora).getRequestContext().put(REQUEST_TIMEOUT, 3000);
-
     }
 
     public WSCorreiosCalculador(final String usuario, final String senha) {
-        this.calculadora = new CalcPrecoPrazoWS().getCalcPrecoPrazoWSSoap();
+        this();
         this.usuario = usuario;
         this.senha = senha;
     }
 
+    public WSCorreiosCalculador setTimeout(final int milliseconds) {
+        final Map<String, Object> requestContext = ((BindingProvider) this.calculadora).getRequestContext();
+        requestContext.put("com.sun.xml.internal.ws.connect.timeout", milliseconds);
+        requestContext.put("com.sun.xml.internal.ws.request.timeout", milliseconds);
+        return this;
+    }
+
     /**
      * Calcula o prazo de envio.
-     * @param servico tipo de serviço.
-     * @param origem cep origem.
-     * @param destino cep destino.
-     * @param data data de envio.
+     *
+     * @param servico   tipo de serviço.
+     * @param origem    cep origem.
+     * @param destino   cep destino.
+     * @param dataEnvio data de envio.
      * @return CServico.
      */
-    public final Optional<CServico> calculaPrazoData(final String servico, final String origem, final String destino, final Calendar data) {
-        final CResultado retornoCorreios;
-
-        String formatedDate = null;
-        if (data != null ){
-            formatedDate = sd.format(data.getTime());
-            retornoCorreios = calculadora.calcPrazoData(servico, origem, destino, formatedDate);
-        } else {
-            retornoCorreios = calculadora.calcPrazo(servico, origem, destino);
-        }
-
+    public final Optional<CServico> calculaPrazoData(final String servico, final String origem, final String destino, final LocalDate dataEnvio) {
+        final CResultado retornoCorreios = dataEnvio == null ? this.calculadora.calcPrazo(servico, origem, destino) : this.calculadora.calcPrazoData(servico, origem, destino, dataEnvio.format(DATE_TIME_FORMATTER));
         final List<CServico> servicos = retornoCorreios.getServicos().getCServico();
         return servicos.isEmpty() ? Optional.empty() : Optional.of(servicos.get(0));
     }
 
     /**
      * Calcula o prazo de envio.
+     *
      * @param servico tipo de serviço.
-     * @param origem cep origem.
+     * @param origem  cep origem.
      * @param destino cep destino.
      * @return CServico.
      */
--- src/test/java/com/github/wmixvideo/correios/WSCorreiosCalculadorTest.java
+++ src/test/java/com/github/wmixvideo/correios/WSCorreiosCalculadorTest.java
@@ -6,15 +6,16 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.Arrays;
-import java.util.Calendar;
 import java.util.List;
 
 public class WSCorreiosCalculadorTest {
 
     @Test
     public void calculaPrazoEntregaSedexMondai() throws IOException {
-        String prazoEntrega = new WSCorreiosCalculador()
+        final String prazoEntrega = new WSCorreiosCalculador()
+                .setTimeout(3000)
                 .calculaPrazo("40010", "88101250", "89893000")
                 .get().getPrazoEntrega();
         Assert.assertTrue(Integer.valueOf(prazoEntrega) > 1);
@@ -22,28 +23,25 @@ public class WSCorreiosCalculadorTest {
 
     @Test
     public void calculaPrazoEntregaPacMondai() throws IOException {
-        String prazoEntrega = new WSCorreiosCalculador()
-                .calculaPrazo("41106", "88101250", "89893000").
-                        get().getPrazoEntrega();
+        final String prazoEntrega = new WSCorreiosCalculador()
+                .setTimeout(3000)
+                .calculaPrazo("41106", "88101250", "89893000")
+                .get().getPrazoEntrega();
         Assert.assertTrue(Integer.valueOf(prazoEntrega) > 1);
     }
 
     @Test
     public void calculaPrazoEntregaPacComData() throws IOException {
-        String prazoEntrega = new WSCorreiosCalculador()
-                .calculaPrazoData("41106", "88101250", "89893000",
-                        Calendar.getInstance()).get().getPrazoEntrega();
+        final String prazoEntrega = new WSCorreiosCalculador()
+                .setTimeout(3000)
+                .calculaPrazoData("41106", "88101250", "89893000", LocalDate.now())
+                .get().getPrazoEntrega();
         Assert.assertTrue(Integer.valueOf(prazoEntrega) > 1);
     }
 
     @Test
     public void calculaPrecosEPrazos() throws IOException {
-        List<CServico> servicos = new WSCorreiosCalculador()
-                .calculaPrecoPrazo(Arrays.asList("40010", "40215", "40290", "41106"),
-                        "88101250", "89893000",
-                        BigDecimal.valueOf(1.5), Formato.CAIXA, BigDecimal.valueOf(25),
-                        BigDecimal.valueOf(25), BigDecimal.valueOf(25), null,
-                        Opcao.NAO, BigDecimal.valueOf(25), Opcao.NAO);
+        final List<CServico> servicos = new WSCorreiosCalculador().calculaPrecoPrazo(Arrays.asList("40010", "40215", "40290", "41106"), "88101250", "89893000", BigDecimal.valueOf(1.5), Formato.CAIXA, BigDecimal.valueOf(25), BigDecimal.valueOf(25), BigDecimal.valueOf(25), null, Opcao.NAO, BigDecimal.valueOf(25), Opcao.NAO);
         Assert.assertEquals(4, servicos.size());
     }
 }
