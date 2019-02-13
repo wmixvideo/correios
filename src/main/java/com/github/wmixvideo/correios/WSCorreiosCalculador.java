b3a1a3dee52e6e920b4f35e8925eafa4350f8839
Mauricio <mauricio@mdantas.net>
2019-02-13 10:34:18 -0200

    Ajuste tratamento timeout construtor webservice

--- src/main/java/com/github/wmixvideo/correios/WSCorreiosCalculador.java
+++ src/main/java/com/github/wmixvideo/correios/WSCorreiosCalculador.java
@@ -17,26 +17,35 @@ import java.util.Optional;
 public class WSCorreiosCalculador {
 
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-    private final CalcPrecoPrazoWSSoap calculadora;
+    private CalcPrecoPrazoWSSoap calculadora;
+    private int timeoutMilliseconds = 15000;
     private String usuario, senha;
 
     public WSCorreiosCalculador() {
-        this.calculadora = new CalcPrecoPrazoWS().getCalcPrecoPrazoWSSoap();
+        this.calculadora = null;
         this.usuario = null;
         this.senha = null;
     }
 
     public WSCorreiosCalculador(final String usuario, final String senha) {
-        this();
         this.usuario = usuario;
         this.senha = senha;
     }
 
     public WSCorreiosCalculador setTimeout(final int milliseconds) {
-        final Map<String, Object> requestContext = ((BindingProvider) this.calculadora).getRequestContext();
-        requestContext.put("com.sun.xml.internal.ws.connect.timeout", milliseconds);
-        requestContext.put("com.sun.xml.internal.ws.request.timeout", milliseconds);
+        this.timeoutMilliseconds = milliseconds;
         return this;
+    }
+
+    private CalcPrecoPrazoWSSoap getCalculadora() {
+        System.setProperty("sun.net.client.defaultConnectTimeout", String.valueOf(this.timeoutMilliseconds));
+        if (this.calculadora == null) {
+            this.calculadora = new CalcPrecoPrazoWS().getCalcPrecoPrazoWSSoap();
+        }
+        final Map<String, Object> requestContext = ((BindingProvider) this.calculadora).getRequestContext();
+        requestContext.put("com.sun.xml.internal.ws.connect.timeout", timeoutMilliseconds);
+        requestContext.put("com.sun.xml.internal.ws.request.timeout", timeoutMilliseconds);
+        return this.calculadora;
     }
 
     /**
@@ -49,7 +58,7 @@ public class WSCorreiosCalculador {
      * @return CServico.
      */
     public final Optional<CServico> calculaPrazoData(final String servico, final String origem, final String destino, final LocalDate dataEnvio) {
-        final CResultado retornoCorreios = dataEnvio == null ? this.calculadora.calcPrazo(servico, origem, destino) : this.calculadora.calcPrazoData(servico, origem, destino, dataEnvio.format(DATE_TIME_FORMATTER));
+        final CResultado retornoCorreios = dataEnvio == null ? this.getCalculadora().calcPrazo(servico, origem, destino) : this.getCalculadora().calcPrazoData(servico, origem, destino, dataEnvio.format(DATE_TIME_FORMATTER));
         final List<CServico> servicos = retornoCorreios.getServicos().getCServico();
         return servicos.isEmpty() ? Optional.empty() : Optional.of(servicos.get(0));
     }
@@ -67,7 +76,7 @@ public class WSCorreiosCalculador {
     }
 
     public final BigDecimal calculaPreco(final String servico, final String origem, final String destino) {
-        final CResultado retornoCorreios = calculadora.calcPreco(this.usuario, this.senha, servico, origem, destino, null, 0, null, null, null, null, null, null, null);
+        final CResultado retornoCorreios = getCalculadora().calcPreco(this.usuario, this.senha, servico, origem, destino, null, 0, null, null, null, null, null, null, null);
         final List<CServico> servicos = retornoCorreios.getServicos().getCServico();
         return servicos.isEmpty() ? null : new BigDecimal(servicos.get(0).getValor());
     }
@@ -76,7 +85,7 @@ public class WSCorreiosCalculador {
         String peso = new DecimalFormat("0.##").format(pesoEmKg);
         String servico = String.join(",", servicos);
 
-        final CResultado retornoCorreios = calculadora.calcPrecoPrazo(this.usuario, this.senha, servico, origem, destino, peso, formato.getCodigo(), comprimentoEmCm, alturaEmCm, larguraEmCm, diametroEmCm, maoPropria.getOpcao(), valorDeclarado, avisoRecebimento.getOpcao());
+        final CResultado retornoCorreios = getCalculadora().calcPrecoPrazo(this.usuario, this.senha, servico, origem, destino, peso, formato.getCodigo(), comprimentoEmCm, alturaEmCm, larguraEmCm, diametroEmCm, maoPropria.getOpcao(), valorDeclarado, avisoRecebimento.getOpcao());
         return retornoCorreios.getServicos().getCServico();
     }
 }
